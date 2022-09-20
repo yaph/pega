b16c33b281b7371278e3b6288b0fa7862a348ea0
Ramiro Gómez <code@ramiro.org>
2022-09-20 23:30:02 +0200

    Add tests

--- tests/test_pega.py
+++ tests/test_pega.py
@@ -2,13 +2,27 @@
 # SPDX-FileCopyrightText: 2022-present Ramiro Gómez <code@ramiro.org>
 #
 # SPDX-License-Identifier: MIT
+import pega
+
 from PIL import Image
 
-from pega import dimensions
+
+BASE_SIZE = (1000, 1000)
 
 
 def test_dimensions():
-    im = Image.new('RGBA', (1000, 1000))
     margins = (840, 40, 40, 840)
-    dim = dimensions(im, margins)
+    dim = pega.dimensions(Image.new('RGBA', BASE_SIZE), margins)
     assert dim == (120, 120)
+
+
+def test_fit_same_size():
+    im = Image.new('RGBA', BASE_SIZE)
+    pega.fit(im, BASE_SIZE)
+    assert BASE_SIZE == im.size
+
+
+def test_fit_smaller_size():
+    im = Image.new('RGBA', BASE_SIZE)
+    pega.fit(im, (100, 100))
+    assert BASE_SIZE == im.size
